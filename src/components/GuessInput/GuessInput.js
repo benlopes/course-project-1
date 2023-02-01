dfc2bc91accfbe767f01afe9f40ff91335a416d8
Ruben Lopes <ruben.lopes@syneoshealth.com>
2023-02-01 10:33:57 -0300

    feat: add guessInput functionality

--- src/components/GuessInput/GuessInput.js
+++ src/components/GuessInput/GuessInput.js
@@ -1,7 +1,30 @@
 import React from "react";
 
 function GuessInput() {
-  return <div />;
+  const [guess, setGuess] = React.useState("");
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    const formattedGuess = guess.toLocaleUpperCase();
+    console.log({ guess: formattedGuess });
+    setGuess("");
+  }
+
+  return (
+    <form className="guess-input-wrapper" onSubmit={(e) => handleSubmit(e)}>
+      <label htmlFor="guess-input">Enter guess:</label>
+      <input
+        id="guess-input"
+        type="text"
+        value={guess}
+        maxLength={5}
+        minLength={5}
+        required={true}
+        onChange={(e) => setGuess(e.target.value)}
+      />
+    </form>
+  );
 }
 
 export default GuessInput;
