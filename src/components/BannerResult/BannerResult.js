b62f76ce3055e78dd1208a2031aef88a8507a88d
Ruben Lopes <ruben.lopes@syneoshealth.com>
2023-02-04 11:17:04 -0300

    feat: BannerResult ready

--- src/components/BannerResult/BannerResult.js
+++ src/components/BannerResult/BannerResult.js
@@ -1,7 +1,20 @@
 import React from "react";
 
-function BannerResult() {
-  return <div />;
+function BannerResult(hasWon, correctWord, numOfGuesses) {
+  return hasWon ? (
+    <div className="happy banner">
+      <p>
+        <strong>Congratulations!</strong> Got it in
+        <strong> {numOfGuesses} guesses</strong>.
+      </p>
+    </div>
+  ) : (
+    <div className="sad banner">
+      <p>
+        Sorry, the correct answer is <strong>{correctWord}</strong>.
+      </p>
+    </div>
+  );
 }
 
 export default BannerResult;
